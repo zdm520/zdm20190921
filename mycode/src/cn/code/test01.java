2729b8ee55459d9a839f0ba7702f44f580b004f6
zdm520 <2774136648@qq.com>
2019-09-26 18:07:37 +0800

    代码

---
+++ mycode/src/cn/code/test01.java
@@ -0,0 +1,17 @@
+
+package cn.code;
+
+public class test01 {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int tmp=0;
+	tmp=a;
+	a=b;
+	b=tmp;
+	System.out.println(a);
+	System.out.println(b);
+}
+
+}
+
---
+++ mycode/src/fsdfs/zdffgh.java
@@ -0,0 +1,13 @@
+package fsdfs;
+
+public class zdffgh {
+public static void main(String[] args) {
+	int a=10;
+	int b=20;
+	int c=30;
+	 int s=(a>b?a:b)>c?(a>b?a:b):c;
+	System.out.println(s);
+}
+	
+	
+}
