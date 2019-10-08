cd6a72cbad20d9461973b094cbbdda810d239d66
zdm520 <2774136648@qq.com>
2019-10-08 15:59:01 +0800

    拆箱与封箱

---
+++ zhao/zzz.java
@@ -0,0 +1,18 @@
+package fgdrger;
+public class zzz {
+	public static void main(String[] args) {
+	    Integer i=1001;
+	    Integer b=1001;
+	    Integer v=null;
+	    int s=new Integer(10000);//其实是s=new Integer(10000).intValue;
+	    int x=v;
+	    System.out.println(x);
+	    System.out.println(s);
+	    System.out.println(i.equals(b)); 
+	    System.out.println(i==b);
+	  //  Integer a=new Integer(1000);
+	    System.out.println(i.toString());
+	}
+	
+}
+
